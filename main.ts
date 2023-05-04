7647c6b9d7e820b8a374a2f612a9772c27e35f3a
Ivan Kraus <91247726+Kraus-Ivan@users.noreply.github.com>
2023-05-04 09:32:16 +0200

    pulsy změněny

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 let servo1 = servoPWM.createServo(AnalogPin.P13);
-servo1.setPulse(1800);
-servo1.setMinPulse(300);
-servo1.setMaxPulse(2800);
+servo1.setPulse(1500);
+
+//změřeno
+servo1.setMinPulse(400);
+servo1.setMaxPulse(2600);
 
 basic.forever(function () {
 	basic.pause(20);
