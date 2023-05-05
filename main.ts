8eb07e39edad3accc274f43e9356f93791fa4a8b
Ivan Kraus <91247726+Kraus-Ivan@users.noreply.github.com>
2023-05-05 13:09:35 +0200

    Aktualizovat pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,64 @@
-let servo1 = servoPWM.createServo(AnalogPin.P13);
-servo1.setPulse(1500);
+class PWMservo {
+    min: number;
+    max: number;
+    current: number;
+    pin: AnalogPin;
 
-//změřeno
-servo1.setMinPulse(400);
-servo1.setMaxPulse(2600);
+    constructor(min: number, max: number, current: number, pin: AnalogPin) {
+        this.min = min;
+        this.max = max;
+        this.current = current;
+        this.pin = pin;
+
+        this.rotate(0);
+    }
+
+    public rotate(howmuchmore: number) {
+        // Opravit použití proměnných třídy
+        if (this.current + howmuchmore > this.max) {
+            this.current = this.max;
+        } else if (this.current + howmuchmore < this.min) {
+            this.current = this.min;
+        } else {
+            this.current += howmuchmore;
+        }
+
+        pins.servoSetPulse(this.pin, this.current);
+        console.log(this.current);
+    }
+}
+
+let switched = false;
+
+pins.setPull(DigitalPin.P16, PinPullMode.PullNone);
+pins.analogWritePin(AnalogPin.P16, 0);
+pins.setPull(DigitalPin.P13, PinPullMode.PullNone);
+pins.analogWritePin(AnalogPin.P13, 0);
+let miniservo = new PWMservo(900, 1900, 1400, AnalogPin.P16);
+let servo1 = new PWMservo(400, 2600, 1500, AnalogPin.P13);
 
 basic.forever(function () {
-	basic.pause(20);
+    basic.pause(20);
+    if(switched){
+        if (input.buttonIsPressed(Button.A)) {
+            miniservo.rotate(20);
+        }
+        if (input.buttonIsPressed(Button.B)) {
+            miniservo.rotate(-20);
+        }
+    }else{
+        if (input.buttonIsPressed(Button.A)) {
+            servo1.rotate(200);
+        }
+        if (input.buttonIsPressed(Button.B)) {
+            servo1.rotate(-200);
+        }
+    }
+
 });
 
+input.onLogoEvent(TouchButtonEvent.Pressed, () => {
 
-input.onButtonPressed(Button.A, () => {
-
-    servo1.setPulseBy(50);
-
-});
-
-input.onButtonPressed(Button.B, () =>{
-
-    servo1.setPulseBy(-50);
-
-});
-
-input.onLogoEvent((TouchButtonEvent.Pressed), () =>{
-
-    
-    basic.showNumber(servo1.getPulse());
+    switched = !switched;
 
 });
